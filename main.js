8709f4ddb8f88f1e6fcb2c6a363aae25233ba208
Vikram Vangari <vikram@revmedia.in>
2019-04-13 21:28:01 +0530

    sd

---
+++ main.js
@@ -0,0 +1,23 @@
+
+$(function() {
+$(document).ready(function(){
+    // if ($(window).innerHeight() = 740){
+    //     $('.content').addClass('android_h');
+    // }
+    // else if ($(window).innerHeight() = 736){
+    //     $('.content').addClass('iphone_h');
+    // }
+    // else if ($(window).innerHeight() = 732){
+    //     $('.content').addClass('android_l');
+    // }
+    // else if ($(window).innerHeight() = 667) {
+    //     $('.content').addClass('iphone_l');
+    // }        
+    // else if ($(window).innerHeight() <= 666  ) {
+    //     $('.content').addClass('android');
+    // }
+    if ($(window).width() >= 412) {
+        $('.content').addClass('android');
+    }
+}); 
+});     
